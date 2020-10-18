26e8eae8c4bccd69e9cc8c1978412353b555818e
lonelyCoder-jrbing <719203729@qq.com>
2020-10-18 13:31:36 +0800

    namedJdbcTempalte 相关操作

--- springdemo/src/main/java/com/springdemo/jdbcdemo/JdbcController.java
+++ springdemo/src/main/java/com/springdemo/jdbcdemo/JdbcController.java
@@ -1,17 +1,23 @@
 package com.springdemo.jdbcdemo;
 
+import lombok.Data;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.BeanPropertyRowMapper;
 import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.namedparam.BeanPropertySqlParameterSource;
+import org.springframework.jdbc.core.namedparam.EmptySqlParameterSource;
+import org.springframework.jdbc.core.namedparam.MapSqlParameterSource;
+import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
 import org.springframework.transaction.TransactionStatus;
 import org.springframework.transaction.support.TransactionCallback;
 import org.springframework.transaction.support.TransactionTemplate;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-
 import javax.annotation.Resource;
 import java.sql.Connection;
 import java.sql.SQLException;
+import java.util.*;
 
 /**
  * create by sumerian on 2020/10/18
@@ -29,6 +35,10 @@ public class JdbcController {
 
     @Resource(name = "txTemplate")
     private TransactionTemplate txTemplate;
+
+    @Autowired
+    private NamedParameterJdbcTemplate namedParameterJdbcTemplate;
+
 
     /****
      * 事务没有回滚的示例
@@ -99,9 +109,9 @@ public class JdbcController {
     @RequestMapping("/druidData3")
     public String runTransactionSamples2() {
         String sql1 = "INSERT INTO star(`id`, `post_id`) VALUES(?, ?)";
-        Object[] args1 = new Object[] {24, 223};
+        Object[] args1 = new Object[]{24, 223};
         String sql2 = "INSERT INTO star(`id`, `post_id`) VALUES(?, ?)";
-        Object[] args2 = new Object[] {26, 112};
+        Object[] args2 = new Object[]{26, 112};
         txTemplate.execute(new TransactionCallback<Object>() {
             @Override
             public Object doInTransaction(TransactionStatus transactionStatus) {
@@ -120,6 +130,7 @@ public class JdbcController {
         });
         return "test2";
     }
+
     /******
      * 带参数的sql执行
      * @return
@@ -127,7 +138,7 @@ public class JdbcController {
     @RequestMapping("/druidData4")
     public String runTransactionSamples3() {
         String sql1 = "UPDATE star set post_id =? where id = ? ";
-        Object[] args1 = new Object[] {223,24};
+        Object[] args1 = new Object[]{223, 24};
         txTemplate.execute(new TransactionCallback<Object>() {
             @Override
             public Object doInTransaction(TransactionStatus transactionStatus) {
@@ -148,10 +159,136 @@ public class JdbcController {
     }
 
 
+    /****
+     * ==============================================================================================
+     * NamedParameterJdbcTemplate 使用方法
+     * ==============================================================================================
+     */
+
+    //使用 map作为参数
+    @RequestMapping("/testkInsert")
+    public Integer testkInsert() {
+        Map<String, Object> paramMap = new HashMap<>();
+        paramMap.put("id", 12);
+        paramMap.put("code", "0001");
+        paramMap.put("name", "张小三");
+        paramMap.put("age", 12);
+        paramMap.put("phoneNo", "18809297467");
+        return namedParameterJdbcTemplate.update(
+                "insert into student(id,code,name,age,phoneNo) values (:id,:code,:name,:age,:phoneNo)",
+                paramMap
+        );
+    }
+
+
+    /****
+     * ==============================================================================================
+     * BeanPropertySqlParameterSource 作为参数
+     * ==============================================================================================
+     */
+
+
+    @Data
+    public static class StudentDTO {
+        private Integer id;
+        private String code;
+        private String name;
+        private int age;
+        private String phoneNo;
+        //getter,setter
+    }
+
+    /******
+     * 使用 map作为参数
+     * @return
+     */
+    @RequestMapping("/testBeanPropertySqlParameterSource")
+    public Integer testBeanPropertySqlParameterSource() {
+        StudentDTO dto = new StudentDTO();//这个DTO为传入数据
+        dto.setId(29);
+        dto.setName("小红");
+        dto.setCode("studentCode");
+        dto.setAge(29);
+        dto.setPhoneNo("18809297467");
+        return namedParameterJdbcTemplate.update("insert into student(id,code,name,age,phoneNo) values (:id,:code,:name,:age,:phoneNo)",
+                new BeanPropertySqlParameterSource(dto));
+
+    }
+
+    /******
+     * 使用 MapSqlParameterSource 作为参数
+     * @return
+     */
+    @RequestMapping("/mapSqlParameterSource")
+    public Integer testMapSqlParameterSource() {
+        MapSqlParameterSource mapSqlParameterSource = new MapSqlParameterSource()
+                .addValue("id", 30)
+                .addValue("code", "studentCode01")
+                .addValue("name", "小王")
+                .addValue("age", 12)
+                .addValue("phoneNo", "18809297467");
+        return namedParameterJdbcTemplate.update("insert into student(id,code,name,age,phoneNo) values (:id,:code,:name,:age,:phoneNo)", mapSqlParameterSource);
+    }
 
 
 
+    /****
+     * ==============================================================================================
+     * 查询
+     * ==============================================================================================
+     */
+    /****
+     * 单行查询
+     * @return
+     */
 
+
+    @RequestMapping("/testQuery")
+    public Integer testQuery() {
+        //查询单个值
+        String name = namedParameterJdbcTemplate.queryForObject("select name from student where id = 1  limit 1 ", EmptySqlParameterSource.INSTANCE, String.class);
+        log.info("name:=>{}", name);
+
+        //BeanPropertyRowMapper会把下划线转化为驼峰属性
+        //结果对象可比实际返回字段多或者少
+        StudentDTO stu = namedParameterJdbcTemplate.queryForObject(
+                "select * from student limit 1", new HashMap<>(), new BeanPropertyRowMapper<StudentDTO>(StudentDTO.class));
+        log.info("testQuery::stu=>{}", stu);
+
+        //返回Map形式的单行数据
+        Map<String, Object> studentMap = namedParameterJdbcTemplate.queryForMap("select * from student limit 1", new HashMap<>());
+        log.info("testQuery::studentMap=>{}", studentMap);
+
+        return namedParameterJdbcTemplate.queryForObject(
+                "select count(*) from student", new HashMap<>(), Integer.class);
+    }
+
+    /****
+     * 返回列表
+     * @return List<String>
+     */
+    @RequestMapping("/queryForList")
+    public List<String> queryForList() {
+        return namedParameterJdbcTemplate.queryForList("select name from student", new HashMap<>(), String.class);
+    }
+
+    /****
+     * 返回多行数据
+     * @return List<StudentDTO>
+     */
+    @RequestMapping("/queryForManyRow")
+    public List<StudentDTO> queryForManyRow() {
+        return namedParameterJdbcTemplate.query("select * from student", new BeanPropertyRowMapper<>(StudentDTO.class));
+    }
+
+    /****
+     * 返回多行数据
+     * @return List<Map < String, Object>>
+     */
+    @RequestMapping("/queryForManyRow1")
+    public List<Map<String, Object>> queryForManyRow1() {
+        return namedParameterJdbcTemplate.queryForList("select * from student", new HashMap<>());
+    }
 
 
 }
